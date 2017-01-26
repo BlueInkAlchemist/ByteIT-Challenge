49d608bd9ce2500e3c34190a40907afb62ca4f27
Josh Loomis <jloomis@blueinkalchemy.com>
2017-01-25 16:51:45 -0800

    Mostly done with Part III of challenge.

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -1,7 +1,7 @@
 <?php
-  add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+  
+  // Init Custom Style
   function my_theme_enqueue_styles() {
-     
      $parent_style = 'parent-style'; // This is 'twentysixteen-style' for the Twenty Sixteen theme.
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
@@ -11,19 +11,269 @@
         wp_get_theme()->get('Version') // Cache-busting goodness.
     );
    }
+   add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-   add_action( 'init', 'create_fb_project_post_type' ); // Adding 'Project' post type to theme
+   // Adding 'Project' CPT to theme
     function create_fb_project_post_type() {
     register_post_type('projects',
             array(
                 'labels' => array(
-                    'name' => __( 'Project\'s' ),
+                    'name' => __( 'Projects' ),
                     'singular_name' => __( 'Project' )
                 ),
             'public' => true,
             'has_archive' => true,
-            'taxonomies' => array('post_tag')
+            'taxonomies' => array('post_tag','category'),
+            'supports' => array( 'title', 'editor', 'thumbnail', 'revisions', 'comments', 'custom-fields', 'excerpt' )
             )
         );
-    }   // Remember to flush & regenerate permalinks!
+    }
+    add_action( 'init', 'create_fb_project_post_type' );    
+
+   // Show 'Project' CPT in loop 
+    function add_custom_post_type_to_query( $query ) {
+        if ( $query->is_home() && $query->is_main_query() ) {
+            $query->set( 'post_type', array('post', 'projects') );
+        }
+    }
+    add_action( 'pre_get_posts', 'add_custom_post_type_to_query' );
+
+    // Show 'Project' CPTs in main archive - functionality TBD
+    /*
+    function namespace_add_custom_types( $query ) {
+        if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+            $query->set( 'post_type', array(
+                'post', 'nav_menu_item', 'projects'
+            ));
+            return $query;
+        }
+    }
+    add_filter( 'pre_get_posts', 'namespace_add_custom_types' );
+    */
+
+    // Custom Field Functionality - Based on author of "Developer's Custom Fields" Plugin - https://sltaylor.co.uk/blog/control-your-own-wordpress-custom-fields/
+
+    if ( !class_exists('myCustomFields') ) {
+ 
+    class myCustomFields {
+        /**
+        * @var  string  $prefix  The prefix for storing custom fields in the postmeta table
+        */
+        var $prefix = '_mcf_';
+        /**
+        * @var  array  $postTypes  An array of public custom post types, plus the standard "post" and "page" - add the custom types you want to include here
+        */
+        var $postTypes = array( "page", "post", "projects" );
+        /**
+        * @var  array  $customFields  Defines the custom fields available
+        */
+        var $customFields = array(
+            array(
+                "name"          => "block-of-text",
+                "title"         => "A block of text",
+                "description"   => "",
+                "type"          => "textarea",
+                "scope"         =>   array( "page" ),
+                "capability"    => "edit_pages"
+            ),
+            array(
+                "name"          => "short-text",
+                "title"         => "A short bit of text",
+                "description"   => "",
+                "type"          =>   "text",
+                "scope"         =>   array( "post" ),
+                "capability"    => "edit_posts"
+            ),
+            array(
+                "name"          => "checkbox",
+                "title"         => "Checkbox",
+                "description"   => "",
+                "type"          => "checkbox",
+                "scope"         =>   array( "post", "page" ),
+                "capability"    => "manage_options"
+            ),
+             array(
+                "name"          => "deadline",
+                "id"            => "deadline",
+                "title"         => "Deadline",
+                "description"   => "",
+                "type"          => "text_date",
+                "scope"         =>   array( "projects" ),
+                "capability"    => "edit_posts"
+            ),
+            array(
+                "name"          => "client",
+                "title"         => "Client",
+                "description"   => "",
+                "type"          =>   "text",
+                "scope"         =>   array( "projects" ),
+                "capability"    => "edit_posts"
+            ), 
+            array(
+                "name"          => "estimate",
+                "title"         => "Estimate",
+                "description"   => "",
+                "type"          =>   "text",
+                "scope"         =>   array( "projects" ),
+                "capability"    => "edit_posts"
+            )
+        );
+        /**
+        * PHP 4 Compatible Constructor
+        */
+        function myCustomFields() { $this->__construct(); }
+        /**
+        * PHP 5 Constructor
+        */
+        function __construct() {
+            add_action( 'admin_menu', array( $this, 'createCustomFields' ) );
+            add_action( 'save_post', array( $this, 'saveCustomFields' ), 1, 2 );
+            // Comment this line out if you want to keep default custom fields meta box
+            add_action( 'do_meta_boxes', array( $this, 'removeDefaultCustomFields' ), 10, 3 );
+        }
+        /**
+        * Remove the default Custom Fields meta box
+        */
+        function removeDefaultCustomFields( $type, $context, $post ) {
+            foreach ( array( 'normal', 'advanced', 'side' ) as $context ) {
+                foreach ( $this->postTypes as $postType ) {
+                    remove_meta_box( 'postcustom', $postType, $context );
+                }
+            }
+        }
+        /**
+        * Create the new Custom Fields meta box
+        */
+        function createCustomFields() {
+            if ( function_exists( 'add_meta_box' ) ) {
+                foreach ( $this->postTypes as $postType ) {
+                    add_meta_box( 'my-custom-fields', 'Custom Fields', array( $this, 'displayCustomFields' ), $postType, 'normal', 'high' );
+                }
+            }
+        }
+        /**
+        * Display the new Custom Fields meta box
+        */
+        function displayCustomFields() {
+            global $post;
+            ?>
+            <div class="form-wrap">
+                <?php
+                wp_nonce_field( 'my-custom-fields', 'my-custom-fields_wpnonce', false, true );
+                foreach ( $this->customFields as $customField ) {
+                    // Check scope
+                    $scope = $customField[ 'scope' ];
+                    $output = false;
+                    foreach ( $scope as $scopeItem ) {
+                        switch ( $scopeItem ) {
+                            default: {
+                                if ( $post->post_type == $scopeItem )
+                                    $output = true;
+                                break;
+                            }
+                        }
+                        if ( $output ) break;
+                    }
+                    // Check capability
+                    if ( !current_user_can( $customField['capability'], $post->ID ) )
+                        $output = false;
+                    // Output if allowed
+                    if ( $output ) { ?>
+                        <div class="form-field form-required">
+                            <?php
+                            switch ( $customField[ 'type' ] ) {
+                                case "checkbox": {
+                                    // Checkbox
+                                    echo '<label for="' . $this->prefix . $customField[ 'name' ] .'" style="display:inline;"><b>' . $customField[ 'title' ] . '</b></label>&nbsp;&nbsp;';
+                                    echo '<input type="checkbox" name="' . $this->prefix . $customField['name'] . '" id="' . $this->prefix . $customField['name'] . '" value="yes"';
+                                    if ( get_post_meta( $post->ID, $this->prefix . $customField['name'], true ) == "yes" )
+                                        echo ' checked="checked"';
+                                    echo '" style="width: auto;" />';
+                                    break;
+                                }
+                                case "textarea":
+                                case "wysiwyg": {
+                                    // Text area
+                                    echo '<label for="' . $this->prefix . $customField[ 'name' ] .'"><b>' . $customField[ 'title' ] . '</b></label>';
+                                    echo '<textarea name="' . $this->prefix . $customField[ 'name' ] . '" id="' . $this->prefix . $customField[ 'name' ] . '" columns="30" rows="3">' . htmlspecialchars( get_post_meta( $post->ID, $this->prefix . $customField[ 'name' ], true ) ) . '</textarea>';
+                                    // WYSIWYG
+                                    if ( $customField[ 'type' ] == "wysiwyg" ) { ?>
+                                        <script type="text/javascript">
+                                            jQuery( document ).ready( function() {
+                                                jQuery( "<?php echo $this->prefix . $customField[ 'name' ]; ?>" ).addClass( "mceEditor" );
+                                                if ( typeof( tinyMCE ) == "object" &amp;&amp; typeof( tinyMCE.execCommand ) == "function" ) {
+                                                    tinyMCE.execCommand( "mceAddControl", false, "<?php echo $this->prefix . $customField[ 'name' ]; ?>" );
+                                                }
+                                            });
+                                        </script>
+                                    <?php }
+                                    break;
+                                }
+                                default: {
+                                    // Plain text field
+                                    echo '<label for="' . $this->prefix . $customField[ 'name' ] .'"><b>' . $customField[ 'title' ] . '</b></label>';
+                                    echo '<input type="text" name="' . $this->prefix . $customField[ 'name' ] . '" id="' . $this->prefix . $customField[ 'name' ] . '" value="' . htmlspecialchars( get_post_meta( $post->ID, $this->prefix . $customField[ 'name' ], true ) ) . '" />';
+                                    break;
+                                }
+                            }
+                            ?>
+                            <?php if ( $customField[ 'description' ] ) echo '<p>' . $customField[ 'description' ] . '</p>'; ?>
+                        </div>
+                    <?php
+                    }
+                } ?>
+            </div>
+            <?php
+        }
+        /**
+        * Save the new Custom Fields values
+        */
+        function saveCustomFields( $post_id, $post ) {
+            if ( !isset( $_POST[ 'my-custom-fields_wpnonce' ] ) || !wp_verify_nonce( $_POST[ 'my-custom-fields_wpnonce' ], 'my-custom-fields' ) )
+                return;
+            if ( !current_user_can( 'edit_post', $post_id ) )
+                return;
+            if ( ! in_array( $post->post_type, $this->postTypes ) )
+                return;
+            foreach ( $this->customFields as $customField ) {
+                if ( current_user_can( $customField['capability'], $post_id ) ) {
+                    if ( isset( $_POST[ $this->prefix . $customField['name'] ] ) && trim( $_POST[ $this->prefix . $customField['name'] ] ) ) {
+                        $value = $_POST[ $this->prefix . $customField['name'] ];
+                        // Auto-paragraphs for any WYSIWYG
+                        if ( $customField['type'] == "wysiwyg" ) $value = wpautop( $value );
+                        update_post_meta( $post_id, $this->prefix . $customField[ 'name' ], $value );
+                    } else {
+                        delete_post_meta( $post_id, $this->prefix . $customField[ 'name' ] );
+                    }
+                }
+            }
+        }
+ 
+        } // End Class
+    
+    } // End if class exists statement
+    
+    // Instantiate the class
+    if ( class_exists('myCustomFields') ) {
+        $myCustomFields_var = new myCustomFields();
+    }
+
+    // Add Datepicker Functionality
+    function enqueue_date_picker(){
+        wp_enqueue_script(
+            'field-date', 
+            get_template_directory_uri() . '/field-date.js', 
+            array('jquery', 'jquery-ui-core', 'jquery-ui-datepicker'),
+            time(),
+            true
+        );  
+
+        wp_enqueue_style( 'jquery-ui-datepicker' );
+    }
+
+    add_action('admin_enqueue_scripts', 'enqueue_date_picker');
+
+    // Remember to flush & regenerate permalinks! 
+    // And toggle comments to refresh those too!
+    // Comments! Things! Excitement!
 ?>
--- wp-content/themes/twentysixteen-child/single-projects.php
+++ wp-content/themes/twentysixteen-child/single-projects.php
@@ -1,17 +1,127 @@
-<?php get_header(); ?>
+<?php 
 
-        <div id="primary" class="site-content">
-            <div id="content" role="main">
+/* 
+  Single Projects Page Template for CPT 
+  (Note copious amounts of capital letters above)
+*/
 
-                <?php while ( have_posts() ) : the_post(); ?>
+get_header(); ?>
 
-                    <?php get_template_part( 'content', 'page' ); ?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
 
-                    <?php comments_template( '', true ); ?>
+			// Include the single post content template.
+			// get_template_part( 'template-parts/content', 'single' );
 
-                <?php endwhile; // end of the loop. ?>
+      // Imported from content-single.php 
+      ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header><!-- .entry-header -->
 
-            </div><!-- #content -->
-        </div><!-- #primary -->
+        <?php twentysixteen_excerpt(); ?>
 
+        <?php twentysixteen_post_thumbnail(); ?>
+
+        <?php 
+        // This looks like a good spot for our custom fields... 
+          $my_id = get_the_ID();
+        // Here's the meta array for trouble-shootin' purposes
+
+          echo '<pre>';
+          print_r(array_values(get_post_meta( $my_id )));
+          echo '</pre>';
+
+        // Here's the simple way. 
+          the_meta();
+
+        // And here's the cool way. 
+        /* 
+          $my_client = get_post_meta( $my_id, 'client', false);
+          $my_deadline = get_post_meta( $my_id, 'deadline', false);
+          $my_estimate = get_post_meta( $my_id, 'estimate', false);
+        
+          if( ! empty( $my_client ) ) {
+            echo '<h3>Client: ' . $my_client . '<h3>';
+            echo '<p>Estimate: ' . $my_estimate . '</p>';
+            echo '<p>Deadline: ' . $my_deadline . '</p>';
+          } 
+        */
+        ?>
+
+        <div class="entry-content">
+          <?php
+            the_content();
+
+            wp_link_pages( array(
+              'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+              'after'       => '</div>',
+              'link_before' => '<span>',
+              'link_after'  => '</span>',
+              'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+              'separator'   => '<span class="screen-reader-text">, </span>',
+            ) );
+
+            if ( '' !== get_the_author_meta( 'description' ) ) {
+              get_template_part( 'template-parts/biography' );
+            }
+          ?>
+        </div><!-- .entry-content -->
+
+        <footer class="entry-footer">
+          <?php twentysixteen_entry_meta(); ?>
+          <?php
+            edit_post_link(
+              sprintf(
+                /* translators: %s: Name of current post */
+                __( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+                get_the_title()
+              ),
+              '<span class="edit-link">',
+              '</span>'
+            );
+          ?>
+        </footer><!-- .entry-footer -->
+      </article><!-- #post-## -->
+
+      <?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+			if ( is_singular( 'attachment' ) ) {
+				// Parent post navigation.
+				the_post_navigation( array(
+					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
+				) );
+			} elseif ( is_singular( 'post' ) ) {
+				// Previous/next post navigation.
+				the_post_navigation( array(
+					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>',
+					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>',
+				) );
+			}
+
+			// End of the loop.
+		endwhile;
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
+
+<!-- The above was pretty much copy-pasta'd from the parent theme's single.php, I regret nothing -->
 <?php get_footer(); ?>
